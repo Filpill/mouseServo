90975f4147d164f17bcc0eecaa415a491c3bd579
Filip Livancic <filiplivancic@gmail.com>
2023-07-15 20:52:20 +0100

    converted scale to map servo angles

--- RaspberryPi/RaspberryPi_MouseServo.py
+++ RaspberryPi/RaspberryPi_MouseServo.py
@@ -7,14 +7,18 @@ from ttkthemes import ThemedTk
 def motion(event):
     x_motion = (window.winfo_pointerx() - window.winfo_rootx()) / window.winfo_width()
     y_motion = 1 - (window.winfo_pointery() - window.winfo_rooty()) / window.winfo_height()
+    x_angle = int((x_motion - 0.5) * 180)
+    y_angle = int((y_motion - 0.5) * 180)
+    yaw_label.config(text='Yaw: '+'{:02d}'.format(x_angle))
+    pitch_label.config(text='Pitch: '+'{:02d}'.format(y_angle))
     x.set(x_motion)
     y.set(y_motion)
-    print('{}, {}'.format(x_motion, y_motion))
+    print('yaw: '+'{:02d}'.format(x_angle)+' | pitch: '+'{:02d}'.format(y_angle))
 
 # Window
 # window = Tk()
 window = ThemedTk(theme="scidblue")
-[width,height]=[475,400]
+[width,height]=[575,500]
 window.geometry(f'{width}x{height}')
 window.title('Servo Controller')
 
@@ -27,11 +31,11 @@ yaw_frame = ttk.Frame(window, width=350, height=75, borderwidth=5, relief = tk.G
 yaw_frame.pack_propagate(False)
 yaw_frame.pack(
     side = 'left',
-    padx = 15
+    padx = 25
     )
 
 x = IntVar()
-yaw_label = Label(yaw_frame, text ='Yaw', font="24")
+yaw_label = Label(yaw_frame, font="24")
 yaw_label.pack()
 
 yaw_bar = ttk.Progressbar(
@@ -50,11 +54,11 @@ pitch_frame = ttk.Frame(window, width=100, height=300, borderwidth=5, relief = t
 pitch_frame.pack_propagate(False)
 pitch_frame.pack(
     side = 'right',
-    padx = 15
+    padx = 25
     )
 
 y = IntVar()
-pitch_label = Label(pitch_frame, text ='Pitch', font="24")
+pitch_label = Label(pitch_frame, font="24")
 pitch_label.pack(side = 'top')
 
 pitch_bar = ttk.Progressbar(
