880d8b77011f8167d553da89e7dc2a75b73c0d2a
Filip Livancic <91389653+Filpill@users.noreply.github.com>
2023-07-15 22:49:36 +0100

    added angular servo, need to fix issues

--- RaspberryPi/RaspberryPi_MouseServo.py
+++ RaspberryPi/RaspberryPi_MouseServo.py
@@ -3,16 +3,32 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from gpiozero import AngularServo
+
 
 def motion(event):
+    
+    # Calculate Position of Mouse Pointer On Screen Based on Screen Resolution
     x_motion = (window.winfo_pointerx() - window.winfo_rootx()) / window.winfo_width()
     y_motion = 1 - (window.winfo_pointery() - window.winfo_rooty()) / window.winfo_height()
+    
+    #Convert to Servo Angle
     x_angle = int((x_motion - 0.5) * 180)
     y_angle = int((y_motion - 0.5) * 180)
     yaw_label.config(text='Yaw: '+'{:02d}'.format(x_angle))
     pitch_label.config(text='Pitch: '+'{:02d}'.format(y_angle))
+    
+    # Set data for the progress bar
     x.set(x_motion)
     y.set(y_motion)
+    
+    # GPIO Pins For Servo
+    yaw_servo = AngularServo(23,min_angle=-90,max_angle=90)
+    pitch_servo = AngularServo(24,min_angle=-90,max_angle=90)
+
+    # Write Angles To The Servos
+    yaw_servo.angle = x_angle
+    pitch_servo.angle = y_angle
     print('yaw: '+'{:02d}'.format(x_angle)+' | pitch: '+'{:02d}'.format(y_angle))
 
 # Window
@@ -22,7 +38,12 @@ window = ThemedTk(theme="scidblue")
 window.geometry(f'{width}x{height}')
 window.title('Servo Controller')
 
-bg = PhotoImage(file=sys.path[0]+"\img\space.png")
+bg_file = "space.png"
+if 'win' in sys.platform:
+    bg_path = sys.path[0]+fr"\img\{bg_file}"
+else:
+    bg_path = sys.path[0]+fr"/img/{bg_file}"
+bg = PhotoImage(file=bg_path)
 my_label = Label(window, image=bg)
 my_label.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -71,7 +92,6 @@ pitch_bar.pack()
 pitch_bar.start()
 pitch_bar.stop()
 pitch_bar.config(variable=y)
-
 
 window.bind('<Motion>', motion)
 window.mainloop()
